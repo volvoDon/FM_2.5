5a6170b4b3ba15e0191689414ce357c55a1f2341
Sam Segal <samsegal@sams-mbp.lan>
2022-12-27 17:39:31 -0700

    V0.1

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,6 @@ use nih_plug::prelude::*;
 use std::sync::Arc;
 use std::f32::consts;
 
-// This is a shortened version of the gain example with most comments removed, check out
-// https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
-// started
 
 struct FmTwo {
     params: Arc<FmTwoParams>,
@@ -21,10 +18,6 @@ struct FmTwo {
 
 #[derive(Params)]
 struct FmTwoParams {
-    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
     #[id = "gain"]
     pub gain: FloatParam,
     #[id = "frequency"]
@@ -59,27 +52,17 @@ impl Default for FmTwo {
 impl Default for FmTwoParams {
     fn default() -> Self {
         Self {
-            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
-            // as decibels is easier to work with, but requires a conversion for every sample.
             gain: FloatParam::new(
                 "Gain",
                 util::db_to_gain(-20.0),
                 FloatRange::Skewed {
                     min: util::db_to_gain(-30.0),
                     max: util::db_to_gain(30.0),
-                    // This makes the range appear as if it was linear when displaying the values as
-                    // decibels
                     factor: FloatRange::gain_skew_factor(-30.0, 30.0),
                 },
             )
-            // Because the gain parameter is stored as linear gain instead of storing the value as
-            // decibels, we need logarithmic smoothing
             .with_smoother(SmoothingStyle::Logarithmic(50.0))
             .with_unit(" dB")
-            // There are many predefined formatters we can use here. If the gain was stored as
-            // decibels instead of as a linear gain value, we could have also used the
-            // `.with_step_size(0.1)` function to get internal rounding.
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
             frequency: FloatParam::new(
@@ -92,8 +75,6 @@ impl Default for FmTwoParams {
                 },
             )
             .with_smoother(SmoothingStyle::Linear(10.0))
-            // We purposely don't specify a step size here, but the parameter should still be
-            // displayed as if it were rounded. This formatter also includes the unit.
             ,
             depth: FloatParam::new(
                 "depth",
@@ -215,9 +196,7 @@ impl Plugin for FmTwo {
 
     const SAMPLE_ACCURATE_AUTOMATION: bool = true;
 
-    // More advanced plugins can use this to run expensive background tasks. See the field's
-    // documentation for more information. `()` means that the plugin does not have any background
-    // tasks.
+    
     type BackgroundTask = ();
 
     fn params(&self) -> Arc<dyn Params> {
@@ -310,7 +289,7 @@ impl Vst3Plugin for FmTwo {
 
     // And don't forget to change these categories, see the docstring on `VST3_CATEGORIES` for more
     // information
-    const VST3_CATEGORIES: &'static str = "Fx|Dynamics";
+    const VST3_CATEGORIES: &'static str = "Instrument|Synth";
 }
 
 nih_export_clap!(FmTwo);
